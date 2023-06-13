93f46e341e31a257a7235aa9f6267f0d512a9eb6
LeJinge <jeremycarmona28@gmail.com>
2023-06-13 17:56:15 +0200

    Récupération de toutes les pages de toutes les catégories. Création d'un csv par catégorie

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from Method.scrap_one_page import scrap_one
 from Method.scrap_one_category import scrap_one_category
+from Method.scrap_all_category import scrap_all_category
+import csv
 
+categorylinks = scrap_all_category()
 book_info = []
 books_info = []
 
-booklinks = scrap_one_category()
+for categorylink in categorylinks:
+    booklinks = scrap_one_category(categorylink)
 
-for booklink in booklinks:
-    book_info = scrap_one(booklink)
-    books_info.append(book_info)
-print(books_info)
+    for booklink in booklinks:
+        book_info = scrap_one(booklink)
+        books_info.append(book_info)
+        csv_category = book_info["category"]
+        filename = f".\Data\Category\{csv_category}.csv"
+        keys = books_info[0].keys()
+        with open(filename, "w", encoding="utf8", newline="") as csvfile:
+            dict_writer = csv.DictWriter(csvfile, keys, delimiter=";")
+            dict_writer.writeheader()
+            dict_writer.writerows(books_info)
+    books_info.clear()
+
+
